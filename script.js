73fe98603571e3aa1b2ab13e910716ebb317e3a6
sasefa <131699944+sasefa@users.noreply.github.com>
2023-10-10 14:11:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,66 @@
+// Get current date
+var currentDate = dayjs().format("dddd, MMMM D");
 
-const currentDayElement = document.getElementById('currentDay');
-const currentDay = dayjs().format('MMMM D, YYYY');
-currentDayElement.textContent = currentDay;
+// Display current date on the page
+document.getElementById("currentDay").textContent = currentDate;
 
-document.addEventListener("DOMContentLoaded", function () {
+// Update time blocks based on current hour
+function updateTimeBlocks() {
   var currentHour = dayjs().format("H");
 
+  // Loop through each time block
+  var timeBlocks = document.getElementsByClassName("time-block");
+  for (var i = 0; i < timeBlocks.length; i++) {
+    var timeBlock = timeBlocks[i];
+    var hour = parseInt(timeBlock.id.split("-")[1]);
+
+    // Check if the hour is in the past, present, or future
+    if (hour < currentHour) {
+      timeBlock.classList.add("past");
+      timeBlock.classList.remove("present", "future");
+    } else if (hour == currentHour) {
+      timeBlock.classList.add("present");
+      timeBlock.classList.remove("past", "future");
+    } else {
+      timeBlock.classList.add("future");
+      timeBlock.classList.remove("past", "present");
+    }
+  }
+}
+
+// Event listener for save buttons
+var saveButtons = document.getElementsByClassName("saveBtn");
+for (var i = 0; i < saveButtons.length; i++) {
+  saveButtons[i].addEventListener("click", saveEvent);
+}
+
+// Save event text in local storage
+function saveEvent(event) {
+  var timeBlock = event.target.closest(".time-block");
+  var hour = timeBlock.id.split("-")[1];
+  var eventText = timeBlock.querySelector(".description").value;
+
+  // Save the event in local storage
+  localStorage.setItem(hour, eventText);
+}
+
+// Load saved events from local storage
+function loadEvents() {
   var timeBlocks = document.getElementsByClassName("time-block");
   for (var i = 0; i < timeBlocks.length; i++) {
     var timeBlock = timeBlocks[i];
     var hour = timeBlock.id.split("-")[1];
+    var savedEvent = localStorage.getItem(hour);
 
-    
-    timeBlock.classList.remove("past");
-    timeBlock.classList.remove("present");
-    timeBlock.classList.remove("future");
-
-    if (hour < currentHour) {
-      timeBlock.classList.add("past");
-    } else if (hour == currentHour) {
-      timeBlock.classList.add("present");
-    } else {
-      timeBlock.classList.add("future");
+    // Set the event text in the time block
+    if (savedEvent) {
+      timeBlock.querySelector(".description").value = savedEvent;
     }
   }
+}
+
+// Call the necessary functions on page load
+window.addEventListener("load", function () {
+  updateTimeBlocks();
+  loadEvents();
 });
-
-document.addEventListener('DOMContentLoaded', function() {
-  var currentHour = dayjs().hour();
-
-  var timeBlocks = document.querySelectorAll('.time-block');
-
-  timeBlocks.forEach(function(timeBlock) {
-    var hour = parseInt(timeBlock.id.split('-')[1]);
-    var description = timeBlock.querySelector('.description');
-    var saveBtn = timeBlock.querySelector('.saveBtn');
-
-    description.value = localStorage.getItem('event-' + hour);
-
-    saveBtn.addEventListener('click', function() {
-      var event = description.value;
-      localStorage.setItem('event-' + hour, event);
-    });
-  });
-});
-
